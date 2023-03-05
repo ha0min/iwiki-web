e418f8ec999d6f8a893a5024ed3385dc60f3e3e6
Max Cheng <ofcoursemax@gmail.com>
2023-03-04 17:34:58 -0800

    feat: add Space page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,7 +2,6 @@ import { PageContainer } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
 import { Card } from 'antd';
 import React from 'react';
-import {SpaceCard} from "@/components/SpaceCard";
 
 const Welcome: React.FC = () => {
   const { initialState } = useModel('@@initialState');
@@ -18,9 +17,7 @@ const Welcome: React.FC = () => {
               ? 'background-image: linear-gradient(75deg, #1A1B1F 0%, #191C1F 100%)'
               : 'background-image: linear-gradient(75deg, #FBFDFF 0%, #F5F7FF 100%)',
         }}
-      >
-        <SpaceCard></SpaceCard>
-      </Card>
+      ></Card>
     </PageContainer>
   );
 };
